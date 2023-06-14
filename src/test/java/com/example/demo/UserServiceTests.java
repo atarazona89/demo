fe879beb21ea1e4fbd95aa6036b9c9096162eeb0
Alejandro Tarazona <atarazona@valio.cl>
2023-06-13 23:28:18 -0400

    wip: working tests

--- src/test/java/com/example/demo/UserServiceTests.java
+++ src/test/java/com/example/demo/UserServiceTests.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.Configs.DemoApplication;
 import com.example.demo.Dtos.Requests.CreateUserRequest;
 import com.example.demo.Dtos.Requests.LoginRequest;
 import com.example.demo.Dtos.Responses.GenericResponse;
 import com.example.demo.Dtos.Responses.LoginResponse;
 import com.example.demo.Entities.UserEntity;
 import com.example.demo.Repositories.UserRepository;
-import com.example.demo.Services.UserService;
+import com.example.demo.Services.Implementation.UserServiceImpl;
 import com.example.demo.utils.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
@@ -21,7 +21,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@SpringBootTest(classes = DemoApplication.class)
 public class UserServiceTests {
 
     @Mock
@@ -30,20 +30,23 @@ public class UserServiceTests {
     @Mock
     private JwtUtil jwtTokenUtil;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
     @InjectMocks
-    private UserService userService;
+    private UserServiceImpl userService;
 
     @Test
     public void testRegisterUser() {
         // Arrange
         MockitoAnnotations.openMocks(this);
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         CreateUserRequest userRegistrationDTO = new CreateUserRequest();
         userRegistrationDTO.setName("John Doe");
         userRegistrationDTO.setEmail("johndoe@example.com");
-        userRegistrationDTO.setPassword("password123");
+        userRegistrationDTO.setPassword("Password12");
 
         when(userRepository.save(any(UserEntity.class))).thenReturn(new UserEntity());
+        when(passwordEncoder.encode(any(String.class))).thenReturn("encoded");
 
         // Act
         userService.createUser(userRegistrationDTO);
@@ -57,12 +60,16 @@ public class UserServiceTests {
         // Arrange
         MockitoAnnotations.openMocks(this);
         String email = "johndoe@example.com";
-        String password = "password123";
+        String password = "Password12";
         UserEntity userEntity = new UserEntity();
+        userEntity.setName("Jon Doe");
         userEntity.setEmail(email);
-        userEntity.setPassword(new BCryptPasswordEncoder().encode(password));
+
+        when(passwordEncoder.encode(any(String.class))).thenReturn("encoded");
+        when(passwordEncoder.matches(any(),any())).thenReturn(Boolean.TRUE);
+        userEntity.setPassword(passwordEncoder.encode(password));
+
         when(userRepository.findByEmail(email)).thenReturn(userEntity);
-        when(jwtTokenUtil.generateToken(userEntity)).thenReturn("testToken");
 
         // Act
         LoginRequest loginRequest = new LoginRequest();
